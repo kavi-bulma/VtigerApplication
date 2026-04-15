dc6c018046acbd390eec63e1ae1b4f7f82ef3e5e
mchet <mchet@DESKTOP-FK0ES2N>
2026-04-15 17:13:20 +0530

    init

---
+++ vrmApplication/src/test/java/CreateOtgModuleTest/CreateOrganizationTest.java
@@ -0,0 +1,123 @@
+package CreateOtgModuleTest;
+
+import java.io.IOException;
+import org.apache.poi.EncryptedDocumentException;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import com.comcast.crm.baseClassUtilities.BaseClassUtilities;
+import com.comcast.crm.fileUtilities.FileUtilities;
+import com.comcast.crm.javaUtilities.JavaUtilities;
+import com.comcast.crm.objectRepo.HomePage;
+import com.comcast.crm.objectRepo.organizationModule.*;
+import com.comcast.webDriverUtilities.WebDriverUtilities;
+
+public class CreateOrganizationTest extends BaseClassUtilities {
+	SoftAssert sa = new SoftAssert();
+	JavaUtilities jutil = new JavaUtilities();
+	FileUtilities fileUtil = new FileUtilities();
+	WebDriverUtilities webUtil = new WebDriverUtilities();
+
+	@Test
+	void createOrg() throws EncryptedDocumentException, IOException, InterruptedException {
+
+		// Create object for homepage and then click on orgLink
+		HomePage hp = new HomePage(driver);
+		hp.orgPage_link.click();
+
+		// Create obj for Organization page and click on + icon to create new org
+		OrganizationPage orgP = new OrganizationPage(driver);
+		orgP.createOrg_button.click();
+
+		// Create fileutil obj to generate random_no & attach it with
+		// orgname(tekPyramid_12)
+		FileUtilities fileUtil = new FileUtilities();
+		String orgName = fileUtil.accessDataFromExcel("orgInfo", 1, 1) + "_" + jutil.getRandomNumber(100);
+
+		// Create obj for crtneworgpage and type org name
+		CreatingNewOrganizationPage crtOrgP = new CreatingNewOrganizationPage(driver);
+		crtOrgP.org_name.sendKeys(orgName);
+
+		// click on save button
+		crtOrgP.save_button.click();
+
+		// Create obj for infopage and verify orginfo displayed or not
+		OrganizationInformationPage infoPage = new OrganizationInformationPage(driver);
+		System.out.println(infoPage.org_information.getText());
+		boolean res = infoPage.org_information.getText().contains(orgName);
+		sa.assertEquals(res, true);
+		Thread.sleep(2000);
+		System.out.println("test case--1 completed");
+	}
+
+	@Test
+	void createOrgWithPhone() throws EncryptedDocumentException, IOException, InterruptedException {
+
+		// Create object for homepage and then click on orgLink
+		HomePage hp = new HomePage(driver);
+		hp.orgPage_link.click();
+
+		// Create obj for Organization page and click on + icon to create new org
+		OrganizationPage orgP = new OrganizationPage(driver);
+		orgP.createOrg_button.click();
+
+		// Create fileutil obj to generate random_no & attach it with
+		// orgname(tekPyramid_12)
+		FileUtilities fileUtil = new FileUtilities();
+		String orgName = fileUtil.accessDataFromExcel("orgInfo", 1, 0) + "_" + jutil.getRandomNumber(100);
+		String phone_num = fileUtil.accessDataFromExcel("orgInfo", 1, 1);
+
+		// Create obj for crtneworgpage and type org name and phone number
+		CreatingNewOrganizationPage crtOrgP = new CreatingNewOrganizationPage(driver);
+		crtOrgP.org_name.sendKeys(orgName);
+		crtOrgP.phone_num.sendKeys(phone_num);
+		crtOrgP.save_button.click();
+
+		// Create obj for infopage and verify orginfo displayed or not
+		OrganizationInformationPage infoPage = new OrganizationInformationPage(driver);
+		boolean res = infoPage.org_information.getText().contains(orgName);
+		sa.assertEquals(res, true);
+		Thread.sleep(2000);
+		System.out.println("test case--2 completed");
+	}
+
+	@Test
+	void createOrgWithIndustryandType() throws EncryptedDocumentException, IOException, InterruptedException {
+
+		// Create object for homepage and then click on orgLink
+		HomePage hp = new HomePage(driver);
+		hp.orgPage_link.click();
+
+		// Create obj for Organization page and click on + icon to create new org
+		OrganizationPage orgP = new OrganizationPage(driver);
+		orgP.createOrg_button.click();
+
+		// Create fileutil obj to generate random_no & attach it with
+		// orgname(tekPyramid_12)
+		String orgName = fileUtil.accessDataFromExcel("orgInfo", 1, 0) + "_" + jutil.getRandomNumber(100);
+		String phone_num = fileUtil.accessDataFromExcel("orgInfo", 1, 1);
+		String industry_value = fileUtil.accessDataFromExcel("orgInfo", 1, 2);
+		String type_value = fileUtil.accessDataFromExcel("orgInfo", 1, 3);
+		System.out.println(industry_value);
+		System.out.println(type_value);
+		System.out.println(orgName);
+		System.out.println(phone_num);
+
+		// Create obj for crtneworgpage and type org name
+		CreatingNewOrganizationPage crtOrgP = new CreatingNewOrganizationPage(driver);
+		crtOrgP.org_name.sendKeys(orgName);
+		crtOrgP.phone_num.sendKeys(phone_num);
+		webUtil.selectDropdownByvisibleText(crtOrgP.select_industry_drop, industry_value);
+		webUtil.selectDropdownByvisibleText(crtOrgP.account_type, type_value);
+
+		crtOrgP.save_button.click();
+
+		// Create obj for infopage and verify orginfo displayed or not
+		OrganizationInformationPage infoPage = new OrganizationInformationPage(driver);
+		System.out.println(infoPage.org_information.getText());
+		boolean res = infoPage.org_information.getText().contains(orgName);
+		sa.assertEquals(res, true);
+		Thread.sleep(2000);
+		System.out.println("test case--3 completed");
+	}
+
+}
---
+++ vrmApplication/src/test/java/createContactTests/CreateContactTest.java
@@ -0,0 +1,84 @@
+package createContactTests;
+
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import com.comcast.crm.baseClassUtilities.BaseClassUtilities;
+import com.comcast.crm.fileUtilities.FileUtilities;
+import com.comcast.crm.javaUtilities.JavaUtilities;
+import com.comcast.crm.objectRepo.contactModule.*;
+import com.comcast.webDriverUtilities.WebDriverUtilities;
+import com.comcast.crm.objectRepo.HomePage;
+
+@Listeners(com.comcast.crm.listenerUtility.ListenerUtilityImplementationClass.class)
+public class CreateContactTest extends BaseClassUtilities {
+
+	SoftAssert sa = new SoftAssert();
+	JavaUtilities jutil = new JavaUtilities();
+	FileUtilities fileUtil = new FileUtilities();
+	WebDriverUtilities webUtil = new WebDriverUtilities();
+
+	@Test
+	public void createContactWithOrg() throws EncryptedDocumentException, IOException {
+
+		/** Place the cursor and click on "Contact" Link **/
+		HomePage hp = new HomePage(driver);
+		hp.contactPage_link.click();
+
+		// Navigate to create new -Contact page by click on "+" image
+		ContactPage cp = new ContactPage(driver);
+		cp.createcontact_button.click();
+
+		/** Create Contacct with mandatory fields **/
+		CreateNewContactPage crtNewContactP = new CreateNewContactPage(driver);
+		String fnameTitle = fileUtil.accessDataFromExcel("contactInfo", 1, 0);
+		String fname = fileUtil.accessDataFromExcel("contactInfo", 1, 1) + "_" + jutil.getRandomNumber(100);
+		String lname = fileUtil.accessDataFromExcel("contactInfo", 1, 1);
+		webUtil.selectDropdownByvisibleText(crtNewContactP.fName_title, fnameTitle);
+		crtNewContactP.fName.sendKeys(fname);
+		crtNewContactP.lName.sendKeys(lname);
+		crtNewContactP.save_button.click();
+
+		/** Verify the ContactInformation **/
+		ContactInformationPage contactInfoP = new ContactInformationPage(driver);
+		String contactInfor = contactInfoP.contact_information.getText();
+		boolean res = contactInfor.contains(fname);
+		sa.assertEquals(res, true);
+	}
+
+	@Test
+	public void createContact() throws EncryptedDocumentException, IOException, InterruptedException {
+
+		/** Place the cursor and click on "Contact" Link **/
+		HomePage hp = new HomePage(driver);
+		hp.contactPage_link.click();
+
+		// Navigate to create new -Contact page by click on "+" image
+		ContactPage cp = new ContactPage(driver);
+		cp.createcontact_button.click();
+
+		/** Create Contacct with mandatory fields **/
+		CreateNewContactPage crtNewContactP = new CreateNewContactPage(driver);
+		String fnameTitle = fileUtil.accessDataFromExcel("contactInfo", 1, 0);
+		String fname = fileUtil.accessDataFromExcel("contactInfo", 1, 1) + "_" + jutil.getRandomNumber(100);
+		String lname = fileUtil.accessDataFromExcel("contactInfo", 1, 2);
+		webUtil.selectDropdownByvisibleText(crtNewContactP.fName_title, fnameTitle);
+		crtNewContactP.fName.sendKeys(fname);
+		crtNewContactP.lName.sendKeys(lname);
+		crtNewContactP.createcontactWithSupportdate();
+		crtNewContactP.save_button.click();
+
+		// select support start and end date
+
+		/** Verify the ContactInformation **/
+		ContactInformationPage contactInfoP = new ContactInformationPage(driver);
+		String contactInfor = contactInfoP.contact_information.getText();
+		boolean res = contactInfor.contains(fname);
+		sa.assertEquals(res, true);
+	}
+
+}
